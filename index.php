3f9c01161113bfcdc309d5dd5091fe7f278c00b7
Joseph <tjogunnupe@gmail.coom>
2023-10-10 18:54:36 -0400

    Second Commit

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TaiwoJoseph Enterprise Employee Database</title>
+    <link rel="stylesheet" href="style.css" type="text/css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+</head>
+<body>
+    <h1 class="title">Employee Database</h1>
+
+    <div class="container">
+        <h2>All Employees of Genjo Bank</h2>
+    <table class="table table-hover table-bordered table-striped">
+        <thead>
+            <tr>
+                <th>
+                    Employee ID
+                </th>
+
+                <th>
+                    First Name
+                </th>
+
+                <th>
+                    Last Name
+                </th>
+
+                <th>
+                    Salary
+                </th>
+            </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <td>3</td>   
+            <td>Rick</td>   
+            <td>Jones</td>   
+            <td>67000</td>    
+        </tr>
+        <tr>
+            <td>5</td>   
+            <td>Samuel</td>   
+            <td>Montage</td>   
+            <td>67000</td>    
+        </tr>
+        </tbody>
+
+    </table>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</body>
+</html>
